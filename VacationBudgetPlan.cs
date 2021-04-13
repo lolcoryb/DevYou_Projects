f3201ae8c9e0a36c61d5e40aeb3254283e026745
lolcoryb <71185428+lolcoryb@users.noreply.github.com>
2021-04-12 22:48:10 -0400

    Add files via upload

---
+++ VacationBudgetPlan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace vacationbudgetplanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string destination;
+
+
+            Console.WriteLine("~Welcome to the Capstone Project~");
+
+            Console.WriteLine("Please enter your name : ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Welcome {name}! Which of these two countries would you rather travel to? Mexico or Jamaica?");
+            destination = Console.ReadLine();
+
+            Console.WriteLine($"{destination} eh? Sounds fun!");
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadKey();
+        }
+    }
+}
